99419a648438fbd82afc12c42be1e8696b04c05b
Alexander Kushko <kushko.alex@gmail.com>
2015-03-28 12:46:29 +0200

    ---

--- public/scripts/fashionIntention/elements/postMenu/postMenu.js
+++ public/scripts/fashionIntention/elements/postMenu/postMenu.js
@@ -6,13 +6,22 @@ FIN.postMenu = function ($object) {
         tp = global.cnCt.tp,
         settings = fin.settings,
         posts = settings.dataModels.posts,
+        $body = document.body,
         $fragment,
         $contentFragment,
+        $monthFragment,
         $link,
         $menuContent,
         $closeButton,
         $postMenuWrapper,
+        $postMenu,
+        $monthsWrapper,
+        postMenuHiddenCssClass = 'hidden',
         build,
+        buildYear,
+        $year,
+        $years = [],
+        months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
         contentBuild,
         u;
 
@@ -20,7 +29,25 @@ FIN.postMenu = function ($object) {
     $contentFragment = global.document.createDocumentFragment();
 
     for (var i = 2013; i < 2016; i++) {
-        contentBuild = tp('postYear', {year: i}, $contentFragment);
+
+        buildYear = tp('postYear', {year: i}, $contentFragment);
+
+        $monthsWrapper = buildYear.monthsWrapper;
+        $year = buildYear.r;
+        $years.push($year);
+
+        $monthFragment = global.document.createDocumentFragment();
+
+        for (var j = 0; j < 12; j++) {
+            tp('postMonth', {title: months[j], index: j, year: i}, $monthFragment);
+        }
+
+        $monthsWrapper.appendChild($monthFragment);
+
+        a9.addEvent($year, eventOnPointerEnd, onYearClick);
+
+        //yearBuild.r.appendChild($monthsWrapper);
+
     }
 
     build = tp('postMenuWrapper', $fragment);
@@ -28,24 +55,93 @@ FIN.postMenu = function ($object) {
     $link = build.toggleLink;
     $menuContent = build.menuContent;
     $closeButton = build.closeButton;
+    $postMenu = build.postMenuContainer;
+
 
     //console.log($link);
 
     $menuContent.appendChild($contentFragment);
 
-    a9.addEvent($link, eventOnPointerEnd, toggleMenu);
-    a9.addEvent($closeButton, eventOnPointerEnd, toggleMenu);
 
     //var $postMenuWrapper = tp('postMenuWrapper').r;
 
-    function toggleMenu() {
-        console.log('toggle');
+    function toggleMenu(e) {
+        if (a9.hasClass($postMenu, postMenuHiddenCssClass)) {
+            a9.removeClass($postMenu, postMenuHiddenCssClass);
+        } else {
+            a9.addClass($postMenu, postMenuHiddenCssClass);
+        }
+        //e.preventDefault();
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        } else {
+            e.cancelBubble = true;
+        }
+
+        //return false;
+        //e.cancelBubble();
     }
+
+    function closeMenu() {
+        if (!a9.hasClass($postMenu, postMenuHiddenCssClass)) {
+            a9.addClass($postMenu, postMenuHiddenCssClass);
+        }
+    }
+
+    function doNotCloseMenu(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        } else {
+            e.cancelBubble = true;
+        }
+    }
+
+    //function openMenu(){
+    //    if(a9.hasClass($postMenu,postMenuHiddenCssClass)){
+    //        a9.removeClass($postMenu,postMenuHiddenCssClass);
+    //    }
+    //}
+
+    function onYearClick(e) {
+        unselectAllYears();
+        triggerYearClick(e.target);
+    }
+
+    function unselectAllYears() {
+        a9.each($years, function ($year) {
+            a9.removeClass($year, 'selected');
+            var $months = a9.$c('months',$year)[0];
+            if ($months) {
+                if (!a9.hasClass($months, 'hidden')) {
+                    a9.addClass($months, 'hidden');
+                }
+            }
+        });
+    }
+
+    function triggerYearClick($target) {
+        //console.log($target);
+        var $year = a9.getParentByClass($target, 'year');
+        var $months = a9.$c('months', $year)[0];
+        if ($months) {
+            a9.removeClass($months, 'hidden');
+            a9.addClass($year, 'selected');
+        }
+
+        //a9.addClass($year, 'selected');
+    }
+
 
     $postMenuWrapper = tp('postMenu').r;
     $postMenuWrapper.appendChild(build.r);
 
     $object.appendChild($postMenuWrapper);
+
+
+    a9.addEvent($link, eventOnPointerEnd, toggleMenu);
+    a9.addEvent($closeButton, eventOnPointerEnd, closeMenu);
+    a9.addEvent($body, eventOnPointerEnd, closeMenu);
+    a9.addEvent($postMenuWrapper, eventOnPointerEnd, doNotCloseMenu);
 
 
     //a9.each(posts, function(post,i){
--- public/scripts/fashionIntention/elements/postMenu/tmpls_postMenu.js
+++ public/scripts/fashionIntention/elements/postMenu/tmpls_postMenu.js
@@ -5,31 +5,38 @@
         u;
 
 
-    tmpls.postYear =function(year){
-        return {c:'year',t:year.year}
-    };
-
-    tmpls.postMonth = function(month){
-        return {c:'month',C:{e:'a',h:'#',t:month}}
-    };
-
-    tmpls.postMenuWrapper = function(){
-        return {c:'post-menu-wrapper',C: [
-            {c: 'post-menu-link', t: 'все посты', n:'toggleLink'},
-            {
-                c: 'post-menu', C: [
-                {c: 'post-menu-triangle'},
-                {c: 'close-button', n:'closeButton'},
-                {c: 'inc-top'},
-                {c: 'all-posts', t: 'все посты'},
-                {c: 'content', n: 'menuContent'},
-                {c: 'inc-bottom'}
+    tmpls.postYear = function (year) {
+        return {
+            c: 'year', C: [
+                {c: 'title', t: year.year},
+                    {c: 'months hidden', n: 'monthsWrapper'}
             ]
-            }]};
+        }
+    };
+
+    tmpls.postMonth = function (month) {
+        return {c: 'month', C: {e: 'a', h: 'posts/' + month.year + '/' + (month.index + 1), t: month.title}}
+    };
+
+    tmpls.postMenuWrapper = function () {
+        return {
+            c: 'post-menu-wrapper', C: [
+                {c: 'post-menu-link', t: 'все посты', n: 'toggleLink'},
+                {
+                    c: 'post-menu hidden', n: 'postMenuContainer', C: [
+                    {c: 'post-menu-triangle'},
+                    {c: 'close-button', n: 'closeButton'},
+                    {c: 'inc-top'},
+                    {c: 'all-posts', t: 'все посты'},
+                    {c: 'content', n: 'menuContent'},
+                    {c: 'inc-bottom'}
+                ]
+                }]
+        };
     };
 
     tmpls.postMenu = function () {
-        return {c:'post-menu-wrapper', n:'postMenuWrapper'};
+        return {c: 'post-menu-wrapper', n: 'postMenuWrapper'};
     };
 
 }(FIN));
