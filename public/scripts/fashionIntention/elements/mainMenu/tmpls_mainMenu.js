46ae314107c7f12f99bce4ddf20fe95b11bb16e0
Alexander Kushko <kushko.alex@gmail.com>
2015-03-28 13:22:45 +0200

    ---

--- public/scripts/fashionIntention/elements/mainMenu/tmpls_mainMenu.js
+++ public/scripts/fashionIntention/elements/mainMenu/tmpls_mainMenu.js
@@ -6,13 +6,17 @@
 
 
     tmpls.mainMenu = function () {
-        return [
-            tmpls.header(),
-
-            {c: 'main-image-content'},
-            tmpls.postMenuWrapper(),
-            tmpls.footer()
-        ];
+        return {
+            c:'main-menu-container',C:{
+                e:'ul', c:'main-menu',C:[
+                    {e:'li',c:'author two-lines',H:'<span>оксана<br>литвиненко</span>'},
+                    {e:'li',c:'press one-line',H:'<a href="">пресса</a>'},
+                    {e:'li',c:'travel one-line',H:'<a href="">путешествия</a>'},
+                    {e:'li',c:'category one-line',H:'<a href=""> категории</a>'},
+                    {e:'li',c:'media one-line',H:'<a href="">медиа</a>'}
+                ]
+            }
+        };
     };
 
 }(FIN));
--- public/scripts/fashionIntention/elements/mainPage/tmpls_mainPage.js
+++ public/scripts/fashionIntention/elements/mainPage/tmpls_mainPage.js
@@ -8,12 +8,96 @@
     tmpls.mainPage = function () {
         return [
             tmpls.header(),
-
-            {c:'main-image-content'},
-            tmpls.postMenu(),
+            {
+                c: 'main-image-content-container', C: [
+                {c: 'main-image-content'},
+                tmpls.postMenu(),
+                tmpls.mainMenu()
+            ]
+            },
+            {
+                c: 'main-content-container', C: [
+                tmpls.postPreviewWrapper({direction: 'left'}),
+                tmpls.postPreviewWrapper({direction: 'left'}),
+                tmpls.postPreviewWrapper({direction: 'right'}),
+                tmpls.postPreviewWrapper({direction: 'right'}),
+                tmpls.clear()
+            ]
+            },
 
             tmpls.footer()
         ];
     };
+
+    tmpls.postPreviewImage = function () {
+        return {c: 'preview-image', C: {c: 'post-preview-triangle'}};
+    };
+
+    tmpls.postPreviewInfo = function () {
+        return {
+            c: 'info', C: [
+                {
+                    c: 'info-content', C: [
+                    {c: 'date', t: '16.11.2015'},
+                    {c: 'title', C: {e: 'a', h: '', t: 'Пять привычек, за которые не стыдно'}},
+                    {
+                        c: 'description',
+                        t: 'Приоткрываем завесу борьбы с нежеланием рано просыпаться приоткрываем завесу борьбы с нежеланием рано просыпаться приоткрываем завесу борьбы с нежеланием рано просыпаться приоткрываем завесу борьбы с нежеланием рано просыпаться просыпаться приоткрываем завесу борьбы с нежеланием рано просыпаться просыпаться приоткрываем завесу борьбы с нежеланием рано просыпаться'
+                    }
+                ]
+                },
+                {
+                    c: 'tags-wrapper', C: [
+                    {c: 'separator'},
+                    {
+                        c: 'tags', C: [
+                        tmpls.tag({title: 'love', url: ''}),
+                        tmpls.tag({title: 'hello', url: ''}),
+                        tmpls.tag({title: 'world', url: ''}),
+                        tmpls.tag({title: 'love', url: ''}),
+                        tmpls.tag({title: 'hello', url: ''}),
+                        tmpls.tag({title: 'world', url: ''}),
+                        tmpls.tag({title: 'the dress is white or blue?', url: ''}),
+                        tmpls.tag({title: 'hello', url: ''}),
+                        tmpls.tag({title: 'world', url: ''}),
+                        tmpls.tag({title: 'world', url: ''}),
+                        tmpls.tag({title: 'world', url: ''}),
+                        tmpls.tag({title: 'world', url: ''})
+                    ]
+                    }
+                ]
+                }
+            ]
+        }
+    };
+
+
+    tmpls.postPreviewWrapper = function (postData) {
+        var wrapperCssClass = 'post-preview-wrapper '+ postData.direction,
+            content = [];
+
+        if (postData.direction == 'left') {
+            content.push(tmpls.postPreviewImage());
+            content.push(tmpls.postPreviewInfo());
+        } else {
+            content.push(tmpls.postPreviewInfo());
+            content.push(tmpls.postPreviewImage());
+        }
+
+        return {
+            c: wrapperCssClass, C: content
+        }
+    };
+
+    tmpls.tag = function (tagData) {
+        return {
+            c: 'tag',
+            C: [{c: 'tag-triangle-wrapper', C: {c: 'tag-triangle'}}, {c: 'tag-content', t: tagData.title}]
+        }
+    };
+
+    tmpls.clear = function () {
+        return {c: 'clear'}
+    }
 
 }(FIN));
